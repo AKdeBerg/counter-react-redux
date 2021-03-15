014cfa16359d935e6e6aae29e1f82a9e647e32b7
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-15 12:13:24 +0600

    Counter component react part, no redux yet

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Counter = () => {
 
-    return Counter;
+    return (
+        <div>
+            <h2>Counter</h2>
+            {/*Here lies the actual counter, we fetch it from state*/}
+            <h3></h3> 
+            {/*we fire an action cretor upon onClick*/}
+            <button onClick={}>Increase</button>
+            <button onClick={}>Decrease</button>
+            <button onClick={}>Reset</button>
+        </div>
+
+    );
 }
 
 export default Counter;
