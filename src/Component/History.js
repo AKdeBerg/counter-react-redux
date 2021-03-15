b4a2dfae55bbf8545314693162e49c2d9899cd27
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-15 12:34:48 +0600

    History Component react part, no redux yet

--- src/Component/History.js
+++ src/Component/History.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const History = () => {
 
-    return History;
+    return (
+        <div>
+            <h2>History</h2>
+            <ul>
+                {/*map over the history state and put them in <li>*/}
+            </ul>
+        </div>
+    );
 }
 
 export default History;
