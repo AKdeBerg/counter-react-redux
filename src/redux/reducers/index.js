f69e6527fcd6c6fb0e57c414a17b6fd13ab690ca
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-17 12:49:51 +0600

    fixing the export issue

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,4 +4,4 @@ import counter from '../reducers/counter';
 
 //HERE WE COMBINE ALL OF OUR REDUCERS
 //INTO A SINGLE OBJECT VIA COMBINEREDUCERS
-export default rootReducers = combineReducers({counter: counter});
+export default combineReducers({counter: counter});
