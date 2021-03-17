57b6b48fff3456ade49b289a37f33ef91bc1eb1a
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-17 15:01:15 +0600

    adding redux devtools

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './component/App';
 import rootReducers from './redux/reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store = {store}>
