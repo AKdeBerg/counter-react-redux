15244472ed1fc058523585e2acee12a02a97a0fe
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-17 15:36:52 +0600

    adding state to History component using react-redux

--- src/Component/History.js
+++ src/Component/History.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const History = () => {
+const History = ({history}) => {
 
     return (
         <div>
             <h2>History</h2>
             <ul>
                 {/*map over the history state and put them in <li>*/}
+                {history.map((count, idx) => {
+                    return (
+                        <li key={idx}>{count}</li>
+                    )
+                })}
             </ul>
         </div>
     );
 }
 
-export default History;
+const mapStateToProp = (state) => {
+
+    return {history: state.counter.history}
+}
+
+export default connect(mapStateToProp, null)(History);
