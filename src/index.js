a897e27deec234b8a75e9d6d3212e1c2db5110e3
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-17 12:50:29 +0600

    adding react-redux functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './component/App';
+import rootReducers from './redux/reducers/index';
+
+const store = createStore(rootReducers);
 
 ReactDOM.render(
-    <App />,
+    <Provider store = {store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );  
