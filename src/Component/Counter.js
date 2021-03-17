61020f68508f45d98d9ab9a022acf362ae5691bf
AKdeBerg <ich.bin.anik@gmail.com>
2021-03-17 15:26:16 +0600

    adding state to the Counter component using react-redux connect

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { decreaseCount, increaseCount, resetCount } from '../redux/actions';
 
-const Counter = () => {
+const Counter = ({counter, increaseCount, decreaseCount, resetCount}) => {
 
     return (
         <div>
             <h2>Counter</h2>
             {/*Here lies the actual counter, we fetch it from state*/}
-            <h3></h3> 
-            {/*we fire an action cretor upon onClick*/}
-            <button onClick={() => {}}>Increase</button>
-            <button onClick={() => {}}>Decrease</button>
-            <button onClick={() => {}}>Reset</button>
+            <h3>{counter}</h3> 
+            {/*we fire an action creator upon onClick*/}
+            <button onClick={increaseCount}>Increase</button>
+            <button onClick={decreaseCount}>Decrease</button>
+            <button onClick={resetCount}>Reset</button>
         </div>
 
     );
 }
 
-export default Counter;
+const mapStateToProp  = (state) => {
+    return {counter: state.counter.count}
+}
+
+export default connect(mapStateToProp, {
+    increaseCount: increaseCount,
+    decreaseCount: decreaseCount,
+    resetCount: resetCount
+})(Counter);
